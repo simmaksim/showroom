ccf1dedb5fbc86f0e5107530d9213aaeb7972c0e
simmaksim <maxim.slizh@mail.ru>
2022-12-30 13:10:16 +0300

    changed project struct; fixed small bugs
    pr comments corrected
    
    Final version of models

---
+++ showroom_project/showroom/abstract_models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+
+class CreateUpdate(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
