883c8925ff8932dd9c3cac4e5f832de9990d2817
simmaksim <maxim.slizh@mail.ru>
2022-12-30 13:10:16 +0300

    changed project struct; fixed small bugs
    pr comments corrected
    
    Final version of models
    
    changed project struct; fixed small bugs
    
    pr comments corrected
    
    models recreated

--- showroom_project/showroom_project/wsgi.py
+++ showroom_project/showroom_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShowroomProject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "showroom_project.settings")
 
 application = get_wsgi_application()
